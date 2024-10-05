e088dfa4b74cdbf7683fb581ee289183e679f755
lausalber <150662642+lausalber@users.noreply.github.com>
2024-10-05 15:54:03 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+
+TcpClient clientSocket = new TcpClient("127.0.0.1", 7);
+Console.WriteLine("Client is ready");
+
+Stream ns = clientSocket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+writer.AutoFlush = true;
+
+string welcome = reader.ReadLine();
+Console.WriteLine(welcome);
+
+while (true)
+{
+    // server asks for method
+    string message = reader.ReadLine();
+    Console.WriteLine(message);
+
+    // chooses which method to use and sends it to the server
+    message = Console.ReadLine();
+    writer.WriteLine(message);
+
+    // server asks for numbers
+    message = reader.ReadLine();
+    Console.WriteLine(message);
+
+    // two numbers sent to server
+    message = Console.ReadLine();
+    writer.WriteLine(message);
+
+    // result sent from server
+    string result = reader.ReadLine();
+    Console.WriteLine(result);
+}
+
+ns.Close();
+clientSocket.Close();
---
+++ Server/ServerService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ServerService
+    {
+        public void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+
+            writer.WriteLine("Welcome to the server!");
+            writer.Flush();
+
+            while (true)
+            {
+                writer.WriteLine("Please type add, subtract, or random (type close to exit):");
+                writer.Flush();
+                string message = reader.ReadLine();
+
+                if (message.ToLower() == "close")
+                {
+                    break;
+                }
+
+                writer.WriteLine("Please enter two numbers separated by a space:");
+                writer.Flush();
+                string numbersMessage = reader.ReadLine();
+
+                double? output = 0;
+
+                if (message.ToLower() == "add")
+                {
+                    output = AddRequest(numbersMessage);
+                    if (output == null)
+                    {
+                        writer.WriteLine("Invalid input, please try again");
+                    }
+                    else
+                    {
+                        writer.WriteLine(output);
+                    }
+                }
+                else if (message.ToLower() == "subtract")
+                {
+                    output = SubtractRequest(numbersMessage);
+                    if (output == null)
+                    {
+                        writer.WriteLine("Invalid input, please try again");
+                    }
+                    else
+                    {
+                        writer.WriteLine(output);
+                    }
+                }
+                else if (message.ToLower() == "random")
+                {
+                    output = RandomRequest(numbersMessage);
+                    if (output == null)
+                    {
+                        writer.WriteLine("Invalid input, please try again. Remember the first number can't be bigger than the second number.");
+                    }
+                    else
+                    {
+                        writer.WriteLine(output);
+                    }
+                }
+                else
+                {
+                    writer.WriteLine("Invalid input. Please type add, subtract, or random (type close to exit):");
+                }
+
+                writer.Flush();
+                Console.WriteLine("Client sent: " + message);
+            }
+            socket.Close();
+        }
+
+        public double? AddRequest(string request)
+        {
+            string[] stn = request.Split(" ");
+
+            if (stn.Length != 2 || !double.TryParse(stn[0], out double x) || !double.TryParse(stn[1], out double y))
+            {
+                return null;
+            }
+
+            return x + y;
+        }
+
+        public double? SubtractRequest(string request)
+        {
+            string[] stn = request.Split(" ");
+
+            if (stn.Length != 2 || !double.TryParse(stn[0], out double x) || !double.TryParse(stn[1], out double y))
+            {
+                return null;
+            }
+
+            return x - y;
+        }
+
+        public int? RandomRequest(string request)
+        {
+            string[] stn = request.Split(" ");
+            if (stn.Length != 2 || !int.TryParse(stn[0], out int x) || !int.TryParse(stn[1], out int y) || (x > y))
+            {
+                return null;
+            }
+            Random random = new Random();
+            return random.Next(x, y + 1);
+        }
+    }
+}
